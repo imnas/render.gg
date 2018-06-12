587974f5d37a5537a808e546a9763f0c3b95a185
jaymontana <nasakthe@gmail.com>
2018-06-12 07:08:39 +0100

    revert redesign

--- src/components/common/header.class.js
+++ src/components/common/header.class.js
@@ -14,7 +14,7 @@ class Header extends Component {
   render() {
     return (
       <div className="headerFlexContainer">
-        <span className="glareHeader"></span>
+        {/* <span className="glareHeader"></span> */}
         <div className="heroTopContainer">
             <nav className="topNav">
                 <div className="navLeft">
